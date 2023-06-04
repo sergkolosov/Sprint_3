584d6c3b04c14334aecc58d4971b7539d61b4658
Sergey Kolosov <serikbay1971@yandex.ru>
2023-06-04 19:19:25 +0400

    Edited the conftest.py

--- conftest.py
+++ conftest.py
@@ -1,14 +1,7 @@
-import random
-import secrets
-
 import pytest
-import time
 
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from data import Urls
-from data import Creds
-from locator import Locators
 
 
 @pytest.fixture
@@ -18,11 +11,3 @@ def driver():
     browser.get(Urls.MAIN_PAGE)
     yield browser
     browser.quit()
-
-
-@pytest.fixture
-def login(driver):
-    """Авторизация"""
-    driver.find_element(*Locators.EMAIL_FIELD).send_keys(*Creds.USER_EMAIL)
-    driver.find_element(*Locators.PASSWORD_FIELD).send_keys(*Creds.USER_PASSWORD)
-    driver.find_element(*Locators.LOGIN_BUTTON).click()
