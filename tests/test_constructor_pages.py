49874a1f0080f49bfb978debb7405ad4347462d5
Sergey Kolosov <serikbay1971@yandex.ru>
2023-06-05 20:41:08 +0400

    optimized assert "flag_selected_tab" has been taken out for a class

--- tests/test_constructor_pages.py
+++ tests/test_constructor_pages.py
@@ -3,31 +3,29 @@ from selenium.webdriver.support.wait import WebDriverWait
 
 from locator import Locators
 
+flag_selected_tab = 'tab_tab_type_current__2BEPc'
+
 
 class TestConstructorPages:
-    flag_selected_tab = 'tab_tab_type_current__2BEPc'
 
     def test_go_to_sauces_partition(self, driver):
         """Проверяем что работает переход к разделу «Соусы»"""
         WebDriverWait(driver, 3).until(EC.element_to_be_clickable(Locators.SAUCES_BUTTON)).click()
 
-        actually_value = self.flag_selected_tab in driver.find_element(*Locators.SAUCES_BUTTON_SELECT).get_attribute('class')
-        expected_value = True
-        assert actually_value == expected_value, f'Ожидалось, что содержание "{self.flag_selected_tab}" в выделенном табе: "{expected_value}", получено "{actually_value}"'
+        tab_classes = driver.find_element(*Locators.FILLINGS_BUTTON_SELECT).get_attribute('class')
+        assert flag_selected_tab in tab_classes, f'Ожидалось, что содержание "{flag_selected_tab}" в выделенном табе: "{flag_selected_tab}", получено "{tab_classes}"'
 
     def test_go_to_fillings_partition(self, driver):
         """Проверяем что работает переход к разделу «Начинки»"""
         WebDriverWait(driver, 3).until(EC.element_to_be_clickable(Locators.FILLINGS_BUTTON)).click()
 
-        actually_value = self.flag_selected_tab in driver.find_element(*Locators.FILLINGS_BUTTON_SELECT).get_attribute('class')
-        expected_value = True
-        assert actually_value == expected_value, f'Ожидалось, что содержание "{self.flag_selected_tab}" в выделенном табе: "{expected_value}", получено "{actually_value}"'
+        tab_classes = driver.find_element(*Locators.FILLINGS_BUTTON_SELECT).get_attribute('class')
+        assert flag_selected_tab in tab_classes, f'Ожидалось, что содержание "{flag_selected_tab}" в выделенном табе: "{flag_selected_tab}", получено "{tab_classes}"'
 
     def test_go_to_buns_partition(self, driver):
         """Проверяем что работает переход к разделу «Булки»"""
         WebDriverWait(driver, 3).until(EC.element_to_be_clickable(Locators.SAUCES_BUTTON)).click()
         driver.find_element(*Locators.BUNS_BUTTON).click()
 
-        actually_value = self.flag_selected_tab in driver.find_element(*Locators.BUNS_BUTTON_SELECT).get_attribute('class')
-        expected_value = True
-        assert actually_value == expected_value, f'Ожидалось, что содержание "{self.flag_selected_tab}" в выделенном табе: "{expected_value}", получено "{actually_value}"'
+        tab_classes = driver.find_element(*Locators.FILLINGS_BUTTON_SELECT).get_attribute('class')
+        assert flag_selected_tab in tab_classes, f'Ожидалось, что содержание "{flag_selected_tab}" в выделенном табе: "{flag_selected_tab}", получено "{tab_classes}"'
